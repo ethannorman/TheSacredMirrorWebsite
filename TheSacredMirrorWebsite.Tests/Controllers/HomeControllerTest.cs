ae89fa875d32bf864fd4545313c2befe6f199988
Ethan <ethannorman@outlook.com>
2018-07-30 15:22:12 -0500

    Add project files.

---
+++ TheSacredMirrorWebsite.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheSacredMirrorWebsite;
+using TheSacredMirrorWebsite.Controllers;
+
+namespace TheSacredMirrorWebsite.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+
+        public void Grimoire()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Grimoire() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Grimoire", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ TheSacredMirrorWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheSacredMirrorWebsite.MvcApplication" Language="C#" %>
